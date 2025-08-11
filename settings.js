a037884bf94b284c89a77ec84bd756fe0571634c
bigspaido <hassanyus390@gmail.com>
2025-08-11 13:21:08 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '✨SPAIDO BOT✨',
+  author: '👑 SPAIDO Hacks',
+  botName: '🤖 SPAIDO BOT',
+  botOwner: 'SPAIDO Hacks', // Your name
+  ownerNumber: '639691465580', // Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: 'public', // can be 'public' or 'private'
+  description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by SPAIDO Hacks.',
+  version: '2.0.0',
+};
+
+module.exports = settings;
