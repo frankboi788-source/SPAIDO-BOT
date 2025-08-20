d9ea10eb7e8b3ccb2c43d7d1d0c911f966dfeb02
frankboi788-source <frankboi788@gmail.com>
2025-08-20 16:16:01 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '👑 SPAIDO Hacks',
   botName: '🤖 SPAIDO BOT',
   botOwner: 'SPAIDO Hacks', // Your name
-  ownerNumber: '639691465580', // Set your number here without + symbol.
+  ownerNumber: '27783583068', // Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: 'public', // can be 'public' or 'private'
   description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by SPAIDO Hacks.',
